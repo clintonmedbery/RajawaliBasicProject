9e9ce817a8e0fba28edc3e07a9cef9ac24b63f13
clintomed <clintonmedbery@Clintons-Mac-Pro-2.local>
2015-04-06 14:14:25 -0400

    Basic Tutorial for Rajawali Android Library

---
+++ app/src/main/java/com/clintonmedbery/rajawalibasicproject/MainActivity.java
@@ -0,0 +1,56 @@
+package com.clintonmedbery.rajawalibasicproject;
+
+import android.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.RajawaliSurfaceView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Renderer renderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RajawaliSurfaceView surface = new RajawaliSurfaceView(this);
+        surface.setFrameRate(60.0);
+        surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
+
+        // Add mSurface to your root view
+        addContentView(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        renderer = new Renderer(this);
+        surface.setSurfaceRenderer(renderer);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/clintonmedbery/rajawalibasicproject/Renderer.java
@@ -0,0 +1,76 @@
+package com.clintonmedbery.rajawalibasicproject;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import org.rajawali3d.lights.DirectionalLight;
+import org.rajawali3d.materials.Material;
+import org.rajawali3d.materials.methods.DiffuseMethod;
+import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.Texture;
+import org.rajawali3d.math.vector.Vector3;
+import org.rajawali3d.primitives.Sphere;
+import org.rajawali3d.renderer.RajawaliRenderer;
+
+/**
+ * Created by clintonmedbery on 4/6/15.
+ */
+public class Renderer extends RajawaliRenderer {
+
+    public Context context;
+
+    private DirectionalLight directionalLight;
+    private Sphere earthSphere;
+
+    public Renderer(Context context) {
+        super(context);
+        this.context = context;
+        setFrameRate(60);
+    }
+
+    public void initScene(){
+
+        directionalLight = new DirectionalLight(1f, .2f, -1.0f);
+        directionalLight.setColor(1.0f, 1.0f, 1.0f);
+        directionalLight.setPower(2);
+        getCurrentScene().addLight(directionalLight);
+
+        Material material = new Material();
+        material.enableLighting(true);
+        material.setDiffuseMethod(new DiffuseMethod.Lambert());
+        material.setColor(0);
+
+        Texture earthTexture = new Texture("Earth", R.drawable.earthtruecolor_nasa_big);
+        try{
+            material.addTexture(earthTexture);
+
+        } catch (ATexture.TextureException error){
+            Log.d("DEBUG", "TEXTURE ERROR");
+        }
+
+        earthSphere = new Sphere(1, 24, 24);
+        earthSphere.setMaterial(material);
+        getCurrentScene().addChild(earthSphere);
+        getCurrentCamera().setZ(4.2f);
+
+    }
+
+
+    @Override
+     public void onRender(final long elapsedTime, final double deltaTime) {
+        super.onRender(elapsedTime, deltaTime);
+        earthSphere.rotate(Vector3.Axis.Y, 1.0);
+    }
+
+
+    public void onTouchEvent(MotionEvent event){
+
+
+    }
+
+    public void onOffsetsChanged(float x, float y, float z, float w, int i, int j){
+
+    }
+}
+
